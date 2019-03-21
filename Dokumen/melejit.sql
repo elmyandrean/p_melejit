a5f415a49f2e81314420a2b01b2c80bbc97278b2
Elmy Andrean <elmy.andrean@gmail.com>
2019-03-21 16:17:14 +0700

    new DB

--- Dokumen/melejit.sql
+++ Dokumen/melejit.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 19, 2019 at 10:27 AM
+-- Generation Time: Mar 21, 2019 at 10:11 AM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.2.13
 
@@ -40,7 +40,9 @@ CREATE TABLE `migrations` (
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (7, '2014_10_12_000000_create_users_table', 1),
-(8, '2014_10_12_100000_create_password_resets_table', 1);
+(8, '2014_10_12_100000_create_password_resets_table', 1),
+(10, '2019_03_20_061604_create_product_holdings_table', 2),
+(12, '2019_03_20_082524_create_product_contents_table', 3);
 
 -- --------------------------------------------------------
 
@@ -53,6 +55,61 @@ CREATE TABLE `password_resets` (
   `token` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_contents`
+--
+
+CREATE TABLE `product_contents` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `product_holding_id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `point` int(11) NOT NULL,
+  `status` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'active',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `product_contents`
+--
+
+INSERT INTO `product_contents` (`id`, `product_holding_id`, `name`, `point`, `status`, `created_at`, `updated_at`) VALUES
+(1, 1, 'Tabungan Mandiri', 5, 'active', '2019-03-20 19:37:32', '2019-03-20 19:37:32'),
+(2, 1, 'MTR (Mandiri Tabungan Rencana)', 10, 'active', '2019-03-20 19:37:32', '2019-03-20 19:37:32'),
+(3, 1, 'MTB (Mandiri Tabungan Bisnis)', 10, 'active', '2019-03-20 19:37:32', '2019-03-20 19:37:32'),
+(4, 5, '-', 15, 'active', '2019-03-20 20:00:11', '2019-03-20 20:00:11'),
+(13, 9, '-', 1, 'deactive', '2019-03-21 00:42:43', '2019-03-21 01:12:13'),
+(16, 9, 'aaa', 1, 'deactive', '2019-03-21 00:47:23', '2019-03-21 01:12:13'),
+(17, 9, 'bbb', 2, 'deactive', '2019-03-21 00:47:23', '2019-03-21 01:12:13'),
+(18, 9, 'aaa', 1, 'deactive', '2019-03-21 01:03:52', '2019-03-21 01:12:13'),
+(19, 9, 'ccc', 3, 'deactive', '2019-03-21 01:03:52', '2019-03-21 01:12:13');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_holdings`
+--
+
+CREATE TABLE `product_holdings` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `menu` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'active',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `product_holdings`
+--
+
+INSERT INTO `product_holdings` (`id`, `name`, `menu`, `status`, `created_at`, `updated_at`) VALUES
+(1, 'Tabungan', 'funding', 'active', '2019-03-20 19:37:32', '2019-03-20 19:37:32'),
+(5, 'New Payroll', 'funding', 'active', '2019-03-20 20:00:11', '2019-03-20 20:00:11'),
+(9, 'test', 'funding', 'deactive', '2019-03-21 00:42:43', '2019-03-21 01:12:13');
 
 -- --------------------------------------------------------
 
@@ -97,6 +154,19 @@ ALTER TABLE `password_resets`
   ADD KEY `password_resets_email_index` (`email`);
 
 --
+-- Indexes for table `product_contents`
+--
+ALTER TABLE `product_contents`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product_contents_product_holding_id_foreign` (`product_holding_id`);
+
+--
+-- Indexes for table `product_holdings`
+--
+ALTER TABLE `product_holdings`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -111,13 +181,35 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `product_contents`
+--
+ALTER TABLE `product_contents`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `product_holdings`
+--
+ALTER TABLE `product_holdings`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `product_contents`
+--
+ALTER TABLE `product_contents`
+  ADD CONSTRAINT `product_contents_product_holding_id_foreign` FOREIGN KEY (`product_holding_id`) REFERENCES `product_holdings` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
