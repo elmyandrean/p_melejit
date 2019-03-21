ec6568b145ed6ef58582c5a833b9795081150333
Elmy Andrean <elmy.andrean@gmail.com>
2019-03-21 23:55:32 +0700

    update DB content

--- Dokumen/melejit.sql
+++ Dokumen/melejit.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 21, 2019 at 10:11 AM
+-- Generation Time: Mar 21, 2019 at 05:55 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.2.13
 
@@ -25,6 +25,26 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `fundings`
+--
+
+CREATE TABLE `fundings` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `branch` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `jabatan` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `customer_name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `account_number` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `deposit` decimal(15,2) NOT NULL,
+  `other` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `migrations`
 --
 
@@ -42,7 +62,8 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (7, '2014_10_12_000000_create_users_table', 1),
 (8, '2014_10_12_100000_create_password_resets_table', 1),
 (10, '2019_03_20_061604_create_product_holdings_table', 2),
-(12, '2019_03_20_082524_create_product_contents_table', 3);
+(12, '2019_03_20_082524_create_product_contents_table', 3),
+(15, '2019_03_21_135743_create_fundings_table', 4);
 
 -- --------------------------------------------------------
 
@@ -85,7 +106,24 @@ INSERT INTO `product_contents` (`id`, `product_holding_id`, `name`, `point`, `st
 (16, 9, 'aaa', 1, 'deactive', '2019-03-21 00:47:23', '2019-03-21 01:12:13'),
 (17, 9, 'bbb', 2, 'deactive', '2019-03-21 00:47:23', '2019-03-21 01:12:13'),
 (18, 9, 'aaa', 1, 'deactive', '2019-03-21 01:03:52', '2019-03-21 01:12:13'),
-(19, 9, 'ccc', 3, 'deactive', '2019-03-21 01:03:52', '2019-03-21 01:12:13');
+(19, 9, 'ccc', 3, 'deactive', '2019-03-21 01:03:52', '2019-03-21 01:12:13'),
+(20, 10, '-', 5, 'active', '2019-03-21 07:00:11', '2019-03-21 07:00:11'),
+(21, 11, '-', 10, 'active', '2019-03-21 07:00:34', '2019-03-21 07:00:34'),
+(22, 12, 'Kredit Kendaraan Bermotor', 5, 'active', '2019-03-21 07:04:54', '2019-03-21 07:04:54'),
+(23, 13, 'Kredit Kendaraan Bermotor', 5, 'active', '2019-03-21 07:05:16', '2019-03-21 07:05:16'),
+(24, 14, '-', 10, 'active', '2019-03-21 07:05:45', '2019-03-21 07:05:45'),
+(25, 15, '-', 10, 'active', '2019-03-21 07:06:12', '2019-03-21 07:06:12'),
+(26, 16, 'Referal KPR', 10, 'active', '2019-03-21 07:06:48', '2019-03-21 07:06:48'),
+(27, 17, 'Referal KPR', 10, 'deactive', '2019-03-21 07:06:49', '2019-03-21 07:07:02'),
+(28, 18, '-', 5, 'active', '2019-03-21 07:07:26', '2019-03-21 07:07:26'),
+(29, 19, '-', 5, 'active', '2019-03-21 07:07:40', '2019-03-21 07:07:40'),
+(30, 20, 'Incoming', 5, 'active', '2019-03-21 07:07:59', '2019-03-21 07:07:59'),
+(31, 21, '-', 5, 'active', '2019-03-21 07:08:14', '2019-03-21 07:08:14'),
+(32, 22, '-', 5, 'active', '2019-03-21 07:08:29', '2019-03-21 07:08:29'),
+(33, 23, 'Akuisisi & Aktivasi', 1, 'active', '2019-03-21 07:08:56', '2019-03-21 07:08:56'),
+(34, 24, 'Akuisisi & Aktivasi', 2, 'active', '2019-03-21 07:09:13', '2019-03-21 07:09:13'),
+(35, 25, 'Akuisisi & Aktivasi', 5, 'active', '2019-03-21 07:09:40', '2019-03-21 07:09:40'),
+(36, 26, 'Akuisisi & Aktivasi', 5, 'active', '2019-03-21 07:10:02', '2019-03-21 07:10:02');
 
 -- --------------------------------------------------------
 
@@ -107,9 +145,26 @@ CREATE TABLE `product_holdings` (
 --
 
 INSERT INTO `product_holdings` (`id`, `name`, `menu`, `status`, `created_at`, `updated_at`) VALUES
-(1, 'Tabungan', 'funding', 'active', '2019-03-20 19:37:32', '2019-03-20 19:37:32'),
-(5, 'New Payroll', 'funding', 'active', '2019-03-20 20:00:11', '2019-03-20 20:00:11'),
-(9, 'test', 'funding', 'deactive', '2019-03-21 00:42:43', '2019-03-21 01:12:13');
+(1, 'Tabungan', 'Funding', 'active', '2019-03-20 19:37:32', '2019-03-20 19:37:32'),
+(5, 'New Payroll', 'Funding', 'active', '2019-03-20 20:00:11', '2019-03-20 20:00:11'),
+(9, 'test', 'Funding', 'deactive', '2019-03-21 00:42:43', '2019-03-21 01:12:13'),
+(10, 'Deposito', 'Funding', 'active', '2019-03-21 07:00:11', '2019-03-21 07:00:11'),
+(11, 'GIRO', 'Funding', 'active', '2019-03-21 07:00:34', '2019-03-21 07:00:34'),
+(12, 'MTF', 'KKB', 'active', '2019-03-21 07:04:54', '2019-03-21 07:04:54'),
+(13, 'MUF', 'KKB', 'active', '2019-03-21 07:05:16', '2019-03-21 07:05:16'),
+(14, 'AXA', 'KKB', 'active', '2019-03-21 07:05:45', '2019-03-21 07:05:45'),
+(15, 'SME', 'Kredit Retail', 'active', '2019-03-21 07:06:12', '2019-03-21 07:06:12'),
+(16, 'Consumer Loan', 'Kredit Retail', 'active', '2019-03-21 07:06:48', '2019-03-21 07:06:48'),
+(17, 'Consumer Loan', 'Kredit Retail', 'deactive', '2019-03-21 07:06:49', '2019-03-21 07:07:02'),
+(18, 'KUM', 'Kredit Retail', 'active', '2019-03-21 07:07:26', '2019-03-21 07:07:26'),
+(19, 'KSM', 'Kredit Retail', 'active', '2019-03-21 07:07:40', '2019-03-21 07:07:40'),
+(20, 'CC', 'Kredit Retail', 'active', '2019-03-21 07:07:59', '2019-03-21 07:07:59'),
+(21, 'KUR', 'Kredit Retail', 'active', '2019-03-21 07:08:14', '2019-03-21 07:08:14'),
+(22, 'EDC', 'Transactional', 'active', '2019-03-21 07:08:29', '2019-03-21 07:08:29'),
+(23, 'Mandiri Online', 'Transactional', 'active', '2019-03-21 07:08:56', '2019-03-21 07:08:56'),
+(24, 'Mandiri Internet Bisnis', 'Transactional', 'active', '2019-03-21 07:09:13', '2019-03-21 07:09:13'),
+(25, 'Mandiri Cash Management', 'Transactional', 'active', '2019-03-21 07:09:40', '2019-03-21 07:09:40'),
+(26, 'Branchless Banking', 'Transactional', 'active', '2019-03-21 07:10:02', '2019-03-21 07:10:02');
 
 -- --------------------------------------------------------
 
@@ -140,6 +195,12 @@ INSERT INTO `users` (`id`, `nip`, `name`, `email`, `phone`, `password`, `positio
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `fundings`
+--
+ALTER TABLE `fundings`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `migrations`
@@ -178,22 +239,28 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `fundings`
+--
+ALTER TABLE `fundings`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `product_contents`
 --
 ALTER TABLE `product_contents`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- AUTO_INCREMENT for table `product_holdings`
 --
 ALTER TABLE `product_holdings`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT for table `users`
